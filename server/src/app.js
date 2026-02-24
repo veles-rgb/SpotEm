995a045fdf613150eb966eb5c51575a30e8a8337
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-24 17:36:54 -0500

    Setup app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+import "dotenv/config";
+import express from "express";
+import routes from "./routes/index.js";
+import cors from "cors";
+
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3001;
 
+app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", routes);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
 });
