7e86e18acc093d46f9dd572f2a0d793d079b18e6
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-24 13:18:57 -0500

    init server

---
+++ server/src/app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.SERVER_PORT;
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
