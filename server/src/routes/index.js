a63265b8d0b5561469d84c28dbf423a20ccb3aa9
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-25 15:19:01 -0500

    Add /run route to index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import levelRouter from "./levels.js";
 import leaderboardRouter from './leaderboard.js';
+import run from './run.js';
 
 const router = express.Router();
 
 router.use("/level", levelRouter);
 router.use("/leaderboard", leaderboardRouter);
+router.use("/run", run);
 
 export default router;
