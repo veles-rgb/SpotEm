5e873d5338103cf0dbe41a24c07a0aac58219b12
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-25 17:28:31 -0500

    Add header component to App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import './App.jsx';
 // Pages
 import Home from './pages/Home';
 import Play from './pages/Play.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play/:levelId" element={<Play />} />
