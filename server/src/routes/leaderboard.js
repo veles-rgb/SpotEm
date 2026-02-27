465d2fe2d3a4ad1f8a62ea1db6cb35e7677962e9
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-02-27 17:17:28 -0500

    Add /leaderboard route for getAllLeaderboards

--- server/src/routes/leaderboard.js
+++ server/src/routes/leaderboard.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { postLeaderboard, getLeaderboard } from '../controllers/leaderboard.js';
+import { postLeaderboard, getLeaderboard, getAllLeaderboards } from '../controllers/leaderboard.js';
 
+// GET api/leaderboard/
+router.get('/', getAllLeaderboards);
 
 // GET api/leaderboard/:levelId
 router.get('/:levelId', getLeaderboard);
